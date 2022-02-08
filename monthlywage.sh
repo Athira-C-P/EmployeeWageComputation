cab29387d2274c487be205bf6bfca34798230369
Athira-C-P <athiracp4422@gmail.com>
2022-02-09 01:20:37 +0530

    Added: monthly wage

---
+++ monthlywage.sh
@@ -0,0 +1,22 @@
+empRatePerHr=20;
+isFullTime=1;
+isPartTime=2;
+maxWorkingDays=20;
+totalSalary=0;
+
+for (( days=1; days<=$maxWorkingDays; days++))
+do
+  empCheck=$((RANDOM%3))
+  case $empCheck in
+    $isFullTime) empHrs=8
+            ;;
+    $isPartTime) empHrs=4
+            ;;
+          *) empHrs=0
+            ;;
+  esac
+  salary=$(($empRatePerHr*$empHrs))
+  totalSalary=$(($totalSalary+$salary))
+done
+
+echo $totalSalary;
